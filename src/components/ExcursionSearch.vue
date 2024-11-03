e66ace34cb326bed9055ca05fb578d29e7af187d
gwyndol1ne <ilia200420042@gmail.com>
2024-11-03 06:16:27 +0300

    Suggestions and short-info add

--- src/components/ExcursionSearch.vue
+++ src/components/ExcursionSearch.vue
@@ -2,12 +2,28 @@
   <div class="search-container">
     <h1>Экскурсии по всему миру</h1>
     <div class="filters">
-      <input
-        class="excursion-name-search"
-        type="text"
-        v-model="title"
-        placeholder="Введите название экскурсии"
-      />
+      <div class="input-container">
+        <input
+          class="excursion-name-search"
+          type="text"
+          v-model="title"
+          placeholder="Введите название экскурсии"
+          @input="showSuggestions = title.length > 0"
+        />
+
+        <ul
+          v-if="showSuggestions && filteredSuggestions.length"
+          class="suggestions-list"
+        >
+          <li
+            v-for="(suggestion, index) in filteredSuggestions"
+            :key="index"
+            @click="selectSuggestion(suggestion)"
+          >
+            {{ suggestion }}
+          </li>
+        </ul>
+      </div>
 
       <select v-model="selectedCity">
         <option value="">Выбрать город</option>
@@ -27,11 +43,16 @@
         :key="excursion.id"
         class="excursion-card"
       >
-        <img
-          :src="excursion.image_big"
-          class="excursion-image"
-          alt="Катинка экскурсии"
-        />
+        <div class="image-container">
+          <img
+            :src="excursion.image_big"
+            class="excursion-image"
+            alt="Катинка экскурсии"
+          />
+          <div class="overlay">
+            <p class="short-info">{{ excursion.short_info }}</p>
+          </div>
+        </div>
         <div class="rating-container">
           <img src="@/assets/star.png" alt="Звезда" class="star-image" />
           <p class="rating-text">
@@ -56,6 +77,7 @@ export default defineComponent({
     const selectedCity = ref("");
     const excursions = ref([]);
     const cities = ref([]);
+    const showSuggestions = ref(false);
 
     const loadExcursions = async () => {
       try {
@@ -73,6 +95,16 @@ export default defineComponent({
       }
     };
 
+    const excursionTitles = computed(() =>
+      excursions.value.map((excursion) => excursion.title)
+    );
+
+    const filteredSuggestions = computed(() =>
+      excursionTitles.value.filter((t) =>
+        t.toLowerCase().includes(title.value.toLowerCase())
+      )
+    );
+
     const filteredExcursions = computed(() => {
       return excursions.value.filter((excursion) => {
         const matchesTitle = excursion.title
@@ -84,6 +116,12 @@ export default defineComponent({
         return matchesTitle && matchesCity;
       });
     });
+
+    const selectSuggestion = (suggestion) => {
+      title.value = suggestion;
+      showSuggestions.value = false;
+    };
+
     onMounted(() => {
       loadExcursions();
       loadCities();
@@ -95,6 +133,9 @@ export default defineComponent({
       excursions,
       cities,
       filteredExcursions,
+      filteredSuggestions,
+      showSuggestions,
+      selectSuggestion,
     };
   },
 });
@@ -116,11 +157,58 @@ export default defineComponent({
   margin: auto;
   gap: 1rem;
 }
-.excursion-name-search {
-  width: 400px;
-  height: 30px;
+.input-container {
+  position: relative;
 }
-
+.excursion-name-search {
+  font-size: 16px;
+  padding-left: 10px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  width: 500px;
+  height: 40px;
+  outline: none;
+}
+.excursion-name-search:focus {
+  border-color: #007bff;
+}
+select {
+  width: 250px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  background-color: #ffffff;
+  color: #333;
+  font-size: 16px;
+  appearance: none;
+  outline: none;
+  cursor: pointer;
+  transition: border-color 0.3s, box-shadow 0.3s;
+}
+select:focus {
+  border-color: #007bff;
+}
+.suggestions-list {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  max-height: 150px;
+  overflow-y: auto;
+  margin-top: 5px;
+  background-color: #ffffff;
+  list-style-type: none;
+  padding: 0;
+  position: absolute;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+}
+.suggestions-list li {
+  padding: 8px 12px;
+  cursor: pointer;
+}
+.suggestions-list li:hover {
+  background-color: #f0f0f0;
+}
 h1 {
   text-align: center;
   margin-bottom: 20px;
@@ -133,7 +221,6 @@ h1 {
   margin-top: 20px;
   margin-left: 30px;
 }
-
 .excursion-card {
   width: 90%;
   display: flex;
@@ -143,14 +230,32 @@ h1 {
   gap: 0.5rem;
   margin-bottom: 10px;
 }
-
 .excursion-image {
   border-radius: 10px;
   width: 100%;
   height: 300px;
   object-fit: cover;
 }
-
+.image-container {
+  position: relative;
+  width: 100%;
+}
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(255, 255, 255, 0.7);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0;
+  transition: opacity 0.3s ease;
+}
+.image-container:hover .overlay {
+  opacity: 1;
+}
 h3,
 p {
   margin: 0;
@@ -161,10 +266,15 @@ p {
   font-size: 15px;
   margin-bottom: 4px;
 }
-
 .star-image {
   width: 16px;
   height: 16px;
   margin-right: 4px;
+}
+.short-info {
+  font-size: 16px;
+  color: #333;
+  text-align: left;
+  padding: 10px;
 }
 </style>
